d6d2dcc335904e8cdd727064a4d342fa6c2d250b
minsuk.jang <minsuk.jang@influential.co.kr>
2023-10-23 12:54:22 +0900

    Detail Fragment의 onAttach와 onDetach 일 때 BottomNavigationView의 Visibility 로직 주석 처리

--- app/src/main/java/com/example/ssatest/DetailFragment.kt
+++ app/src/main/java/com/example/ssatest/DetailFragment.kt
@@ -1,15 +1,28 @@
 package com.example.ssatest
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.ssatest.base.BaseFragment
 import com.example.ssatest.databinding.FragmentDetailBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        //requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_view)?.isVisible = false
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        //requireActivity().findViewById<BottomNavigationView>(R.id.bottom_navigation_view)?.isVisible = true
     }
 
     override fun back() {
